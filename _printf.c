7e6ededce01ecf87260084cb54979b109a7a3bcb
maryamazria <maryamazria4@gmail.com>
2023-03-26 14:09:15 -0700

    creating printd.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf -formatted output conversion and print data.
+ * @format: input string.
+ *
+ *return: number of chars printed .
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+
+}
