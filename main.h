1cfc641a7819d5547d222d63eb261c3cc6d6e7b5
maryamazria <maryamazria4@gmail.com>
2023-03-26 14:29:11 -0700

    creat print char function, buffer and identifier funct.

--- main.h
+++ main.h
@@ -19,4 +19,10 @@ typedef struct print
 	int (*f)(va_list, char *,unsigned int);
 } print_t;
 
+int _printf(const char *format, ...);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+
 #endif
